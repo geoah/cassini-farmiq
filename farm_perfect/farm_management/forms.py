16682cf19d772da2b9ce71f28604753d5e307e27
George Antoniadis <george@noodles.gr>
2024-09-14 15:52:51 +0300

    feat: improve date selection

--- farm_perfect/farm_management/forms.py
+++ farm_perfect/farm_management/forms.py
@@ -14,8 +14,15 @@ class SeasonForm(forms.ModelForm):
     class Meta:
         model = Season
         fields = ['crop', 'start_date', 'end_date']
+        widgets = {
+            'start_date': forms.DateInput(attrs={'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'type': 'date'}),
+        }
 
 class SeasonalEventForm(forms.ModelForm):
     class Meta:
         model = SeasonalEvent
         fields = ['event_date', 'description']
+        widgets = {
+            'event_date': forms.DateInput(attrs={'type': 'date'}),
+        }
